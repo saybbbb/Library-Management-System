ded910dff65e5fa8c0bdeac2b3a01232826e7000
Storm <kjialopier@gmail.com>
2024-11-27 00:29:48 +0800

    Change name into LoginForm

--- src/LoginForm.java
+++ src/LoginForm.java
@@ -2,14 +2,14 @@ import javax.swing.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
-public class LoginGUI {
+public class LoginForm {
     private JPanel mainPanel;
     private JTextField usernameField;
     private JPasswordField passwordField;
     private JButton loginButton;
     private JLabel messageLabel;
 
-    public LoginGUI() {
+    public LoginForm() {
         loginButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
@@ -21,7 +21,7 @@ public class LoginGUI {
                     JOptionPane.showMessageDialog(mainPanel, "Login Successful!");
                     messageLabel.setText("");
                 } else {
-                    messageLabel.setText("Invalid username or password.");
+                    messageLabel.setText("Invalid username or password");
                 }
             }
         });
@@ -29,7 +29,7 @@ public class LoginGUI {
 
     public static void main(String[] args) {
         JFrame frame = new JFrame("Library Management System");
-        frame.setContentPane(new LoginGUI().mainPanel);
+        frame.setContentPane(new LoginForm().mainPanel);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.pack();
         frame.setVisible(true);
